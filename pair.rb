18a2c040e4a40b7647531c7afe81ac21c47e49b6
“YOUR <2drum4fun@gmail.com>
2019-02-20 21:12:39 -0600

    print numbers 0 to 11

--- pair.rb
+++ pair.rb
@@ -1,12 +1,20 @@
-foods = []
+# foods = []
 
-5.times do
-  puts "Name one of your favorite foods:"
-  foods << gets.chomp
+# 5.times do
+#   puts "Name one of your favorite foods:"
+#   foods << gets.chomp
+# end
+
+# i = 1
+# foods.each do |food|
+#   puts "#{i}. #{food}"
+#   i += 1 
+# end
+
+count = 0
+
+11.times do
+  p count
+  count+= 1
 end
 
-i = 1
-foods.each do |food|
-  puts "#{i}. #{food}"
-  i += 1 
-end
