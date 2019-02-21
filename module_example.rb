c85f4bfc6deaa23605d0205d6b2be88a5dc45d83
“YOUR <2drum4fun@gmail.com>
2019-02-20 20:52:29 -0600

    add five foods loop

--- module_example.rb
+++ module_example.rb
@@ -1,33 +1,8 @@
-class Vehicle
+module class Vehicle
    def initialize
     @speed = 0
     @direction = 'north'
-  end
 
-  def brake
-    @speed = 0
-  end
-
-  def accelerate
-    @speed += 10
-  end
-
-  def turn(new_direction)
-    @direction = new_direction
-  end
-end
-
-class Car < Vehicle
-  def honk_horn
-    puts "Beeeeeeep!"
-  end
-end
-
-class Bike < Vehicle
-  def ring_bell
-    puts "Ring ring!"
-  end
-end
 
 class Vehicle
   def initialize
---
+++ pair.rb
@@ -0,0 +1,8 @@
+foods = []
+
+5.times do
+  puts "Name one of your favorite foods:"
+  foods << gets.chomp
+end
+
+p foods
