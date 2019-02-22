f3e92f750ef3af2e8953883ef12ea31e29dda778
“YOUR <2drum4fun@gmail.com>
2019-02-21 18:36:27 -0600

    add Scottie named Jackson

--- store/store_item.rb
+++ store/store_item.rb
@@ -1,20 +1,28 @@
-dog1 = {:breed => "Scottie", :color => "black", :price => 400}
-dog2 = {:breed => "Rottie", :color => "black/tan", :price => 500}
-dog3 = {:breed => "German Shepherd", :color => "tan/black", :price => 800}
+dog1 = {:breed => "Scottie", :color => "black", :name => "Jackson", :price => 400}
+# dog2 = {:breed => "Rottie", :color => "black/tan", :price => 500}
+# dog3 = {:breed => "German Shepherd", :color => "tan/black", :price => 800}
 
 
-p "Dog1 is a #{dog1[:color]} #{dog1[:breed]} that costs $#{dog1[:price]}"
-p "Dog2 is a #{dog2[:color]} #{dog2[:breed]} that costs $#{dog2[:price]}"
-p "Dog3 is a #{dog3[:color]} #{dog3[:breed]} that costs $#{dog3[:price]}"
+p "Dog1 is a #{dog1[:color]} #{dog1[:breed]} named #{dog1[:name]} that costs $#{dog1[:price]}"
+# p "Dog2 is a #{dog2[:color]} #{dog2[:breed]} that costs $#{dog2[:price]}"
+# p "Dog3 is a #{dog3[:color]} #{dog3[:breed]} that costs $#{dog3[:price]}"
 
 class Dog
   attr_reader :breed, :color, :price
+  attr_writer :name
 
   def initialize(input_options)
     @breed = input_options[:breed]
     @color = input_options[:color]
     @price = input_options[:price]
+    @name = input_options[:name]
   end
+
+  dog1 = Dog.new(breed: "Scottie", color: "black", name: "Jackson", price: 400 )
+
+  # def print_info
+  #   p "Dog1 is a #{dog1[:color]} #{dog1[:breed]} named #{dog1[:name]} that costs $#{dog1[:price]}"
+  # end
 
   # def input_breed
   #   @input_breed
@@ -33,33 +41,33 @@ class Dog
   # end
 end
 
-class Terrier < Dog
-  def initialize(input_options)
+# # class Terrier < Dog
+# #   def initialize(input_options)
     
-  end
+# #   end
   
-  def ratter
+# #   def ratter
     
-  end 
-end
+#   end 
+# end
 
-dog1 = Dog.new(
-  {
-    :breed => "Scottie",
-    :color => "black",
-    :price => 400
-  }
-  )
+# dog1 = Dog.new(
+#   {
+#     :breed => "Scottie",
+#     :color => "black",
+#     :price => 400
+#   }
+#   )
 
-dog2 = Dog.new(
-  {
-    :breed => "Rottie",
-    :color => "black/tan",
-    :price => 500
-  }
-  )
+# dog2 = Dog.new(
+#   {
+#     :breed => "Rottie",
+#     :color => "black/tan",
+#     :price => 500
+#   }
+#   )
 
-p dog1
-p dog2
+# p dog1
+# p dog2
 
 
