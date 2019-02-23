d3b18cc8da08a20caf0005656f378c7775263c11
“YOUR <2drum4fun@gmail.com>
2019-02-23 16:02:56 -0600

    add setter method to change first name

--- workers/employee.rb
+++ workers/employee.rb
@@ -26,6 +26,26 @@ class Employee
     @active = input_active
   end
 
+  def first_name
+    @first_name
+  end
+
+  def last_name
+    @last_name
+  end
+
+  def salary
+    @salary
+  end
+
+  def salary
+    @salary
+  end
+
+  def first_name=(input_first_name)
+    @first_name = input_first_name 
+  end
+
   def print_info
     p "#{@first_name} #{@last_name} makes $#{@salary} per year."
   end
@@ -36,14 +56,17 @@ class Employee
 end
 
 employee1 = Employee.new("James", "Jones", 1000000, false)
-employee1.print_info
+p employee1.first_name
+employee1.first_name = "Tim"
+p employee1.first_name
+
 employee1.give_annual_raise
 employee1.print_info
 
-employee2 = Employee.new("David", "Koresh", 500000, false)
-employee2.print_info
-employee2.give_annual_raise
-employee2.print_info
+# employee2 = Employee.new("David", "Koresh", 500000, false)
+# employee2.print_info
+# employee2.give_annual_raise
+# employee2.print_info
 
 
 
