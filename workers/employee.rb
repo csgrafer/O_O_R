31addbaa76d30ef2dd8a12bbd4747eab2a5409bd
“YOUR <2drum4fun@gmail.com>
2019-02-23 15:10:24 -0600

    add give a raise method

--- workers/employee.rb
+++ workers/employee.rb
@@ -29,12 +29,21 @@ class Employee
   def print_info
     p "#{@first_name} #{@last_name} makes $#{@salary} per year."
   end
+
+  def give_annual_raise
+    @salary *= 1.05
+  end
 end
 
 employee1 = Employee.new("James", "Jones", 1000000, false)
-employee2 = Employee.new("David", "Koresh", 500000, false)
-
 employee1.print_info
+employee1.give_annual_raise
+employee1.print_info
+
+employee2 = Employee.new("David", "Koresh", 500000, false)
 employee2.print_info
+employee2.give_annual_raise
+employee2.print_info
+
 
 
