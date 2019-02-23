a9f59e1908682e18ab59c1ed9ffe62951707d823
“YOUR <2drum4fun@gmail.com>
2019-02-23 14:52:56 -0600

    model employee using class

--- workers/employee.rb
+++ workers/employee.rb
@@ -9,9 +9,32 @@
 
 # model usin hash
 
-employee1 = { "first_name" => "James", "last_name" => "Jones", "salary" => 1000000, "active" => false }
-employee2 = { :first_name => "David", :last_name => "Koresh", :salary => 500000, :active => false }
+# employee1 = { "first_name" => "James", "last_name" => "Jones", "salary" => 1000000, "active" => false }
+# employee2 = { :first_name => "David", :last_name => "Koresh", :salary => 500000, :active => false }
 
-p "#{employee1['first_name']} #{employee1['last_name']} made $#{employee1['salary']} per year. He is no longer with the company."
-p "#{employee2[:first_name]} #{employee2[:last_name]} made $#{employee2[:salary]} per year. He is no longer employeed either."
+# p "#{employee1['first_name']} #{employee1['last_name']} made $#{employee1['salary']} per year. He is no longer with the company."
+# p "#{employee2[:first_name]} #{employee2[:last_name]} made $#{employee2[:salary]} per year. He is no longer employeed either."
+
+
+# model employee using class
+
+class Employee
+  def initialize(input_first_name, input_last_name, input_salary, input_active)
+    @first_name = input_first_name
+    @last_name = input_last_name
+    @salary = input_salary
+    @active = input_active
+  end
+
+  def print_info
+    p "#{@first_name} #{@last_name} makes $#{@salary} per year."
+  end
+end
+
+employee1 = Employee.new("James", "Jones", 1000000, false)
+employee2 = Employee.new("David", "Koresh", 500000, false)
+
+employee1.print_info
+employee2.print_info
+
 
