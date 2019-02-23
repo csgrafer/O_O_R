6ef8f968c2e995899f7b308bb3c760382a43d52e
“YOUR <2drum4fun@gmail.com>
2019-02-23 16:46:14 -0600

    add manager class

--- workers/employee.rb
+++ workers/employee.rb
@@ -18,7 +18,62 @@
 
 # model employee using class
 
-class Employee
+# class Employee
+#   attr_reader :first_name, :last_name, :salary
+#   attr_writer :first_name
+
+#   def initialize(input_options)
+#     @first_name = input_options[:first_name]
+#     @last_name = input_options[:last_name]
+#     @salary = input_options[:salary]
+#     @active = input_options[:active]
+#   end
+
+#   # def first_name
+#   #   @first_name
+#   # end
+
+#   # def last_name
+#   #   @last_name
+#   # end
+
+#   # def salary
+#   #   @salary
+#   # end
+
+#   # def salary
+#   #   @salary
+#   # end
+
+#   def first_name=(input_first_name)
+#     @first_name = input_first_name 
+#   end
+
+#   def print_info
+#     p "#{@first_name} #{@last_name} makes $#{@salary} per year."
+#   end
+
+#   def give_annual_raise
+#     @salary *= 1.05
+#   end
+# end
+
+# # employee1 = Employee.new( :last_name => "Koresh", :salary => 500000, :active => false, :first_name => "David")
+# employee1 = Employee.new(last_name: "Koresh", salary: 500000, active: false, first_name: "David")
+
+# p employee1.first_name
+# employee1.first_name = "Tim"
+# p employee1.first_name
+
+# employee1.give_annual_raise
+# employee1.print_info
+
+# employee2 = Employee.new("David", "Koresh", 500000, false)
+# employee2.print_info
+# employee2.give_annual_raise
+# employee2.print_info
+
+class Manager
   attr_reader :first_name, :last_name, :salary
   attr_writer :first_name
 
@@ -28,22 +83,6 @@ class Employee
     @salary = input_options[:salary]
     @active = input_options[:active]
   end
-
-  # def first_name
-  #   @first_name
-  # end
-
-  # def last_name
-  #   @last_name
-  # end
-
-  # def salary
-  #   @salary
-  # end
-
-  # def salary
-  #   @salary
-  # end
 
   def first_name=(input_first_name)
     @first_name = input_first_name 
@@ -56,22 +95,19 @@ class Employee
   def give_annual_raise
     @salary *= 1.05
   end
+
+  def send_report
+    p "will send a report"
+    # write code for report
+    p "just sent the report"
+  end
 end
 
-# employee1 = Employee.new( :last_name => "Koresh", :salary => 500000, :active => false, :first_name => "David")
-employee1 = Employee.new(last_name: "Koresh", salary: 500000, active: false, first_name: "David")
-
-p employee1.first_name
-employee1.first_name = "Tim"
-p employee1.first_name
-
-employee1.give_annual_raise
-employee1.print_info
-
-# employee2 = Employee.new("David", "Koresh", 500000, false)
-# employee2.print_info
-# employee2.give_annual_raise
-# employee2.print_info
+manager = Manager.new(last_name: "Gacy", salary: 100000, active: false, first_name: "John")
+manager.print_info
+manager.send_report
+manager.give_annual_raise
+manager.print_info
 
 
 
