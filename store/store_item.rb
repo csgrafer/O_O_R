1f858e9ab7b941502410f736e32af42886987a8d
“YOUR <2drum4fun@gmail.com>
2019-02-24 06:37:56 -0600

    add Terrier class

--- store/store_item.rb
+++ store/store_item.rb
@@ -1,75 +1,48 @@
-dog1 = {:breed => "Scottie", :color => "black", :name => "Jackson", :price => 400}
-dog2 = {:breed => "Rottie", :color => "black/tan", :name => "Spike", :price => 500}
-dog3 = {:breed => "Shepherd", :color => "tan/black", :name => "Sarge", :price => 800}
 
 
-p "Dog1 is a #{dog1[:color]} #{dog1[:breed]} named #{dog1[:name]} that costs $#{dog1[:price]}"
-p "Dog2 is a #{dog2[:color]} #{dog2[:breed]} named #{dog2[:name]} that costs $#{dog2[:price]}"
-p "Dog3 is a #{dog3[:color]} #{dog3[:breed]} named #{dog3[:name]} that costs $#{dog3[:price]}"
+# class Dog
+#   attr_reader :breed, :color, :price
+#   attr_writer :name
 
-class Dog
-  attr_reader :breed, :color, :price
-  attr_writer :name
+#   def initialize(input_options)
+#     @breed = input_options[:breed]
+#     @color = input_options[:color]
+#     @price = input_options[:price]
+#     @name = input_options[:name]
+#   end
+  
+#   def print_info
+#     "This dog is a #{@color} #{@breed} named #{@name} that costs $#{@price}"
+#   end
+# end
+
+#   dog1 = Dog.new(breed: "Scottie", color: "black", name: "Jackson", price: 400 )
+#   dog2 = Dog.new(breed: "Rottie", color: "black/tan", name: "Spike", price: 500 )
+#   dog3 = Dog.new(breed: "Shepherd", color: "tan/black", name: "Sarge", price: 800 )
+
+# p dog1.print_info
+# p dog2.print_info
+# p dog3.print_info
+
+class Terrier
+  attr_reader :breed, :color, :price, :name, :purpose
 
   def initialize(input_options)
     @breed = input_options[:breed]
     @color = input_options[:color]
     @price = input_options[:price]
     @name = input_options[:name]
+    @purpose = input_options[:purpose] 
   end
-
-  dog1 = Dog.new(breed: "Scottie", color: "black", name: "Jackson", price: 400 )
-  dog2 = Dog.new(breed: "Rottie", color: "black/tan", name: "Spike", price: 500 )
-  dog3 = Dog.new(breed: "Shepherd", color: "tan/black", name: "Sarge", price: 800 )
-
-  # def print_info
-  #   p "Dog1 is a #{dog1[:color]} #{dog1[:breed]} named #{dog1[:name]} that costs $#{dog1[:price]}"
-  # end
-
-  # def input_breed
-  #   @input_breed
-  # end
-
-  # def input_color
-  #   @input_color
-  # end
-
-  # def input_price
-  #   @input_price
-  # end
-
-  # def print_info
-  #   p "This dog is a #{}"
-  # end
+  
+  def print_info
+    p "This dog is a #{@color} #{@breed} named #{@name}. He costs $#{@price}, and is an excellent #{@purpose}."
+  end
 end
 
-# # class Terrier < Dog
-# #   def initialize(input_options)
-    
-# #   end
-  
-# #   def ratter
-    
-#   end 
-# end
+terrier1 = Terrier.new(breed: "Scottie", color: "black", name: "Jackson", price: 400, purpose: "ratter" )
 
-# dog1 = Dog.new(
-#   {
-#     :breed => "Scottie",
-#     :color => "black",
-#     :price => 400
-#   }
-#   )
+terrier1.print_info
 
-# dog2 = Dog.new(
-#   {
-#     :breed => "Rottie",
-#     :color => "black/tan",
-#     :price => 500
-#   }
-#   )
-
-# p dog1
-# p dog2
 
 
